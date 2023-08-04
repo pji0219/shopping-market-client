790ed79a24d5fe77738c906da496853228b913dc
pji0219 <pji219@naver.com>
2023-08-04 23:48:35 +0900

    feat: 더미 데이터로 의류 상품 조회 테스트

--- src/clothes/page/Clothes.tsx
+++ src/clothes/page/Clothes.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Clothes() {
-  return <div>Clothes</div>;
-}
+// type Products = {
+//   category: string;
+//   description: string;
+//   id: string;
+//   image: string;
+//   options: string;
+//   price: number;
+//   title: string;
+// };
+
+const Clothes: React.FC = () => {
+  const [products, setProducts] = useState<unknown[] | undefined>();
+
+  useEffect(() => {
+    fetch('/data/products.json')
+      .then(res => res.json())
+      .then(data => {
+        const datas = Object.entries(data);
+        setProducts(datas);
+        console.log(products);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const productList = products?.map((product: any, index) => {
+    return (
+      <li key={index}>
+        <img src={product.image} alt='image' />
+      </li>
+    );
+  });
+
+  return <div>{productList}</div>;
+};
+
+export default Clothes;
